1c4d15dd3352bdbbe4b05c36c14582449920f7e7
Kemuel Batista <kemuellima20@gmail.com>
2023-08-29 06:31:21 -0300

    feature: Api Completa

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
@@ -33,7 +34,10 @@ export class CreateUserUseCase {
     // constructor(usersRepository: IUsersRepository){
     //     this.usersRepository = usersRepository;
     // }
-    constructor(private usersRepository: IUsersRepository){}
+    constructor(
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider
+    ){}
 
     async execute(data: ICreateUserRequestDTO){
         const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
@@ -45,5 +49,18 @@ export class CreateUserUseCase {
         const user = new User(data);
 
         await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name, 
+                email: data.email
+            },
+            from: {
+                name: 'Equipe do meu App',
+                email: 'equipe@meuapp.com'
+            },
+            subject: "Seja bem vindo",
+            body:  '<p>Você já pode fazer login em nossa plataforma</p>'
+        })
     }
 }
